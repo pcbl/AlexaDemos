553059669fec36230cb91ed762973da6c9537d65
Pedro Castelo Branco Lourenco <pedro_castelo@hotmail.com>
2018-11-20 15:22:21 +0100

    Adding JokeIntent

---
+++ MyRobot.Common/Joker/JokeMessage.cs
@@ -0,0 +1,51 @@
+﻿using Alexa.NET;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using MyRobot.Alexa;
+using MyRobot.Common;
+using System.ComponentModel.Composition;
+using System.Text;
+
+namespace MyRobot.Joker
+{
+    [Export(typeof(IMessage))]
+    [ExportMetadata("Intent", "JokeIntent")]
+    class JokeMessage : BaseMessage
+    {
+        public override SkillResponse Process(IntentRequest request)
+        {
+            var responseText = new StringBuilder();
+            //We will use SSML as reponse format
+            //https://developer.amazon.com/docs/custom-skills/speech-synthesis-markup-language-ssml-reference.html
+            responseText.Append("<speak>");
+            JokesRepository repo = new JokesRepository();
+            var categorySlot = request.Intent.Slots["JokeCategory"];
+            var category = string.Empty;
+            string jokeCategory = categorySlot.Value;
+            if(jokeCategory!=null)
+            {
+                //Performing similarity search!
+                jokeCategory = JokesRepository.SelectProperCategory(jokeCategory);
+            }
+            var joke = JokesRepository.NextJoke(jokeCategory);
+
+            if (joke != null)
+            { 
+                responseText.Append("<audio src = 'https://s3.amazonaws.com/ask-soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_positive_response_01.mp3'/>");
+                responseText.Append(joke.JokeText);
+                responseText.Append("<break/>");
+            }
+            else
+            {
+                responseText.Append("<audio src='soundbank://soundlibrary/cartoon/amzn_sfx_boing_long_1x_01'/>");
+                responseText.Append(Ssml.SayAs("Ops! No Joke found!", "interjection"));
+                responseText.Append("<break/>");
+            }
+
+            responseText.Append("</speak>");
+            SsmlOutputSpeech speech = new SsmlOutputSpeech();
+            speech.Ssml = responseText.ToString();
+            return ResponseBuilder.Tell(speech);
+        }
+    }
+}
---
+++ MyRobot.Common/Joker/JokesRepository.cs
@@ -0,0 +1,60 @@
+﻿using DuoVia.FuzzyStrings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyRobot.Joker
+{
+    public class JokesRepository
+    {
+        static List<Joke> _jokes;
+
+        static JokesRepository()
+        {
+            _jokes = new List<Joke>();
+            _jokes.Add(new Joke("Mouse", "What mouse was a Roman emperor ? Julius Cheeser !"));
+            _jokes.Add(new Joke("TV", ",Why can't nanyone stay angry long with an actress? Because she always makes up."));
+            _jokes.Add(new Joke("Computer", "This customer comes into the computer store. I'm looking for a mystery Adventure Game with lots of graphics. You know, something really challenging. Well, replied the clerk, Have you tried Windows ME?"));
+        }
+
+        public class Joke
+        {
+            public Joke(string category,string jokeText)
+            {
+                Category = category;
+                JokeText = jokeText;
+            }
+            public string Category { get; set; }
+            public string JokeText { get; set; }
+        }
+
+        public static string SelectProperCategory(string categoryToSearch)
+        {
+            //We will do a similarity search in order to minimize spoken differences!
+            //Cool Project: https://github.com/tylerjensen/duovia-fuzzystrings
+            return _jokes.Select(joke=>joke.Category).OrderByDescending(category => category.FuzzyMatch(categoryToSearch)).FirstOrDefault();
+        }
+
+        public static Joke NextJoke(string category)
+        {
+            try
+            {
+                return _jokes
+                        .Where(joke => joke.Category == category)
+                        .OrderBy(joke => Guid.NewGuid()) // quick and Dirtz random sort! 
+                        .FirstOrDefault();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static Joke NextJoke()
+        {
+            return _jokes
+                    .OrderBy(joke => Guid.NewGuid()) // quick and Dirtz random sort! 
+                    .FirstOrDefault();
+        }
+    }
+}
